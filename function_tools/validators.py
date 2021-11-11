fdbef08af9a355854e6e38c1f3a98560e1c29cb1
a.danilenko <a.danilenko@bars.group>
2021-11-11 13:28:30 +0300

    BOBUH-18382 Добавлен __init__ в BaseValidator

--- function_tools/validators.py
+++ function_tools/validators.py
@@ -2,6 +2,10 @@ class BaseValidator:
     """
     Базовый класс для создания валидатора
     """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+
     def validate(self, runnable):
         """
         Точка входа работы валидатора.
